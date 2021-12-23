22995b135b4e98259fffaaa84f39fce8eb98f577
Magomed Khadzhiev <tyrneni@yandex.ru>
2021-12-24 00:49:15 +0300

    факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
   // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  let factorialSum = 1;
+
+  for (let i = 1; i <= n; i++) {
+    factorialSum = factorialSum * i;
+  }
+  return factorialSum;
 }
