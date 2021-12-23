883b376ee46e7961fe5dad3207324b3c1024b977
Magomed Khadzhiev <tyrneni@yandex.ru>
2021-12-24 02:28:58 +0300

    Факториал не решен по тупой ошидке

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,14 +1,12 @@
 function factorial(n) {
-
-  // ваш код...
-  if (n == 0 || n == 1) {
-    return 1;
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result;
   }
-  let factorialSum = 1;
 
   for (let i = 1; i <= n; i++) {
-    factorialSum = factorialSum * i;
+    result = result * i;
   }
-  return factorialSum;
 
+  return result;
 }
