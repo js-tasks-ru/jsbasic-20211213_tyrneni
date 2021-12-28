ad539d59e9479511cc90931c243d23621c4b6ba6
Magomed Khadzhiev <tyrneni@yandex.ru>
2021-12-28 15:07:04 +0300

    описание

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+
+  for (let income in salaries) {
+    if (typeof (salaries[income]) == 'number' &&
+              salaries[income] !== 'NaN' &&
+              salaries[income] !== 'Infinity' && 
+              salaries[income] !== '-Infinity') {
+
+      sum = sum + salaries[income];
+    }
+  }
+  return sum;
 }
