bae1903108d7e240d0b31a766afe916b14c0f4d3
Magomed Khadzhiev <tyrneni@yandex.ru>
2021-12-30 02:00:07 +0300

    антиспам '1xbet', 'xxx'

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,7 @@
 function checkSpam(str) {
   // ваш код...
   str = str.toLowerCase();
-  let spamList = ['1xbet now', 'free xxxxx'];
+  let spamList = ['1xbet', 'xxx'];
 
   return spamList.includes(str);
 }
