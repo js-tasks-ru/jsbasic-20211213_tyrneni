599d522e92d72eeadca77211048546eefc99e7e3
Magomed Khadzhiev <tyrneni@yandex.ru>
2021-12-30 22:29:40 +0300

    3 модуль 4 таск

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
 function showSalary(users, age) {
   // ваш код...
+
+  let filtered = users.filter(user => user.age <= age);
+
+  return (filtered.map((item, index) => {
+    if (index == (filtered.length - 1)) {
+      return `${item.name}, ${item.balance}`;
+    } else {
+      return `${item.name}, ${item.balance}\n`;
+    }
+  }).join(""));
+
 }
