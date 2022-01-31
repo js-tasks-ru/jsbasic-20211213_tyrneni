5d284afc9ad7997cd84068e0024babb48ac1d060
Magomed Khadzhiev <tyrneni@yandex.ru>
2022-02-01 02:55:23 +0300

    0 modul

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,8 @@
 export default function promiseClick(button) {
   // ваш код...
+  console.log(button);
+
+  button.addEventListener('click', (event) => {
+    new Promise((resolve) => { resolve(event); });
+  }, { once: true });
 }
